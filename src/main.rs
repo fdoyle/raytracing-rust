5151267094c80344eaaac6b6d440b324f2eb5785
frankd <fdoyl001@gmail.com>
2021-09-11 22:09:22 -0500

    create gradient

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,24 @@ use std::fs::File;
 use std::io::Write;
 use std::process::Command;
 
-const IMAGE_WIDTH: i32 = 2;
-const IMAGE_HEIGHT: i32 = 3;
+const IMAGE_WIDTH: i32 = 256;
+const IMAGE_HEIGHT: i32 = 256;
 
 
 fn main() -> std::io::Result<()> {
     let file_name = "./image.ppm";
-    println!("Hello, world!");
     let mut file = File::create(file_name)?;
     file.write_all(b"P3\n")?;
-    file.write_all(b"3 2\n")?;
+    file.write_fmt(format_args!("{} {}\n", IMAGE_WIDTH, IMAGE_HEIGHT))?;
     file.write_all(b"255\n")?;
-    file.write_all(b"255 0 0 0 255 0 0 0 255\n")?;
-    file.write_all(b"255 255 0 255 255 255 0 0 0\n")?;
+    for row in (1..IMAGE_HEIGHT+1).rev() {
+        for column in 0..IMAGE_WIDTH {
+            let r: i32 = ((column as f64 / (IMAGE_WIDTH-1) as f64) * 255.999) as i32;
+            let g: i32 = ((row as f64 / (IMAGE_HEIGHT-1) as f64) * 255.999) as i32;
+            let b: i32 = (0.25 * 255.999) as i32;
+            file.write_fmt(format_args!("{} {} {}\n", r, g, b))?;
+        }
+    }
 
 
     Command::new("open")
