4f67b705abf6b93ed76d4ee521d94a8e850d088f
frankd <fdoyl001@gmail.com>
2021-09-11 21:44:41 -0500

    first image

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@ use std::fs::File;
 use std::io::Write;
 use std::process::Command;
 
-fn main() -> std::io::Result<()>  {
+const IMAGE_WIDTH: i32 = 2;
+const IMAGE_HEIGHT: i32 = 3;
+
+
+fn main() -> std::io::Result<()> {
     let file_name = "./image.ppm";
     println!("Hello, world!");
     let mut file = File::create(file_name)?;
-    file.write_all(b"Hello World updates")?;
-
+    file.write_all(b"P3\n")?;
+    file.write_all(b"3 2\n")?;
+    file.write_all(b"255\n")?;
+    file.write_all(b"255 0 0 0 255 0 0 0 255\n")?;
+    file.write_all(b"255 255 0 255 255 255 0 0 0\n")?;
 
 
     Command::new("open")
@@ -15,5 +22,4 @@ fn main() -> std::io::Result<()>  {
         .output();
 
     Ok(())
-
 }
