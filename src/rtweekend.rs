856175907e95fa04491819d4ba5023e100f5a7b2
frankd <fdoyl001@gmail.com>
2021-10-24 15:08:48 -0500

    add rand methods

--- src/rtweekend.rs
+++ src/rtweekend.rs
@@ -1,6 +1,14 @@
 pub const infinity: f64 = f64::INFINITY;
 pub const pi: f64 = 3.1415926535897932385;
 
-fn degrees_to_radians(degrees: f64) -> f64 {
+pub fn degrees_to_radians(degrees: f64) -> f64 {
     return degrees * pi / 180.0;
+}
+
+pub fn random_double() -> f64 {
+    return rand::random();
+}
+
+pub fn random_within_range(min: f64, max: f64) -> f64 {
+    return min + (max - min) * random_double();
 }
